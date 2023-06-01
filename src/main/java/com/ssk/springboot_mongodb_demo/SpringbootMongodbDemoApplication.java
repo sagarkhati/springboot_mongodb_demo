1ec4ac4c5d8a233c13c9724e5f6551c8ffb3572a
Sagar Singh Khati <sonu06sagar@gmail.com>
2023-06-01 23:12:21 +0530

    tested deleteAll, save, findAll and findByLastName method

--- src/main/java/com/ssk/springboot_mongodb_demo/SpringbootMongodbDemoApplication.java
+++ src/main/java/com/ssk/springboot_mongodb_demo/SpringbootMongodbDemoApplication.java
@@ -1,13 +1,50 @@
 package com.ssk.springboot_mongodb_demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ssk.springboot_mongodb_demo.model.Customer;
+import com.ssk.springboot_mongodb_demo.repository.CustomerRepository;
+
 @SpringBootApplication
-public class SpringbootMongodbDemoApplication {
+public class SpringbootMongodbDemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private CustomerRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootMongodbDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		// delete existing records
+		repository.deleteAll();
+
+		// save a couple of customers
+		repository.save(new Customer("Alice", "Smith"));
+		repository.save(new Customer("Bob", "Smith"));
+		repository.save(new Customer("Tony", "Stark"));
+		repository.save(new Customer("Peter", "Parker"));
+
+		// fetch all customers
+		System.out.println("Customers found with findAll():");
+		System.out.println("-------------------------------");
+		for (Customer customer : repository.findAll()) {
+			System.out.println(customer);
+		}
+		System.out.println();
+
+		// fetch customers on condition base
+		System.out.println("Customers found with findByLastName('Smith'):");
+		System.out.println("--------------------------------");
+		for (Customer customer : repository.findByLastName("Smith")) {
+			System.out.println(customer);
+		}
+
 	}
 
 }
